adf3dd9817362f5bfd708b45fcdc3b11809c681c
Oran Nahoum <saintoran@gmail.com>
2019-01-05 15:41:03 +0200

    FUCK GUY

---
+++ classifier.py
@@ -0,0 +1,47 @@
+import hw3_utils
+
+
+class knn_classifier(hw3_utils.abstract_classifier):
+
+    def __init__(self, k, data, labels):
+        self.k = k
+        self.data = data
+        self.labels = labels
+
+    def classify(self, features):
+        '''
+        classify a new set of features
+        :param features: the list of feature to classify
+        :return: a tagging of the given features (1 or 0)
+        '''
+
+        labels_copy = self.labels.copy()
+        distance_list = [hw3_utils.euclidian_distance(features, obj) for obj in self.data]
+
+        counter = 0
+        for i in range(self.k):
+            # get the min value index and check if it's classified
+            index = distance_list.index(min(distance_list))
+            if labels_copy[index] == True:
+                counter += 1
+            distance_list.pop(index)
+            labels_copy.pop(index)
+        if float(counter / self.k) > 0.5:
+            return 0  # there are more sick people (we are closer to more than half True objs in classify matrix)
+        return 1
+
+
+class knn_factory(hw3_utils.abstract_classifier_factory):
+
+    def __init__(self, k):
+        self.k = k
+
+    def train(self, data, labels):
+        '''
+        train a classifier
+        :param data: a list of lists that represents the features that the classifier will be trained with
+        :param labels: a list that represents  the labels that the classifier will be trained with
+        :return: abstruct_classifier object
+        '''
+
+        return knn_classifier(self.k, data, labels)
--- hw3_utils.py
+++ hw3_utils.py
@@ -3,8 +3,6 @@ import pickle
 import numpy as np
 
 
-
-
 def load_data(path=r'data/Data.pickle'):
     '''
     return the dataset that will be used in HW 3
@@ -14,10 +12,9 @@ def load_data(path=r'data/Data.pickle'):
     :returns: a tuple train_features, train_labels ,test_features
     features - a numpy matrix where  the ith raw is the feature vector of patient i.
     '''
-    with open(path,'rb') as f:
+    with open(path, 'rb') as f:
         train_features, train_labels, test_features = pickle.load(f)
-    return train_features, train_labels ,test_features
-
+    return train_features, train_labels, test_features
 
 
 def write_prediction(pred, path='results.data'):
@@ -39,6 +36,7 @@ class abstract_classifier_factory:
     '''
     an abstruct class for classifier factory
     '''
+
     def train(self, data, labels):
         '''
         train a classifier
@@ -63,3 +61,111 @@ class abstract_classifier:
         raise Exception('Not implemented')
 
 
+def euclidian_distance(object1, object2):
+    result = map(lambda x, y: (x - y) ** 2, object1, object2)
+    return sum(result) ** 0.5
+
+
+def split_crosscheck_groups(dataset, num_folds):
+    # extract data and labels from data.
+    data = dataset[0]
+    labels = dataset[1]
+
+    true_list = []
+    false_list = []
+    folds = []
+
+    data_size = len(data)
+
+    for i in range(data_size):
+        if labels[i] == True:
+            true_list.append(data[i])
+        else:
+            false_list.append(data[i])
+
+    true_list_size = len(true_list)
+    false_list_size = len(false_list)
+
+    # shuffle the true and false lists.
+    np.random.shuffle(true_list)
+    np.random.shuffle(false_list)
+
+    '''
+    there will be cast to int which will keep equal amount of true and false classifiers 
+    in each fold and will not treat extra classifiers to keep the ratio equal
+    '''
+
+    num_of_true_in_fold = int(true_list_size / num_folds)
+    num_of_false_in_fold = int(false_list_size / num_folds)
+
+    for i in range(num_folds):
+        fold = []
+        for _ in range(num_of_true_in_fold):
+            fold.append((true_list.pop(0), True))  # add the relative number of true in each folder
+        for _ in range(num_of_false_in_fold):
+            fold.append((false_list.pop(0), False))  # add the relative number of false in each folder
+        np.random.shuffle(fold)
+        folds.append(fold)
+
+    for i in range(num_folds):
+        file_name = 'data/ecg_fold_' + str(i + 1) + '.pickle'
+        with open(file_name, 'wb') as fold_data_file:
+            # break the tuples into the desired matrixs
+            matrix1 = np.array([obj[0] for obj in folds[i]])
+            matrix2 = [obj[1] for obj in folds[i]]
+            matrix3 = np.array([[]])
+            p = pickle.dumps((matrix1, matrix2, matrix3))
+            fold_data_file.write(p)
+
+
+def evaluate(classifier_factory, k):
+    # load all folds
+    folds = [load_data('data/ecg_fold_' + str(i + 1) + '.pickle') for i in range(k)]
+
+    accuracies = []
+    errors = []
+    for i in range(k):
+        # choose 1 group to be test group all others will be train groups
+        test_data = folds[i][0]
+        test_labels = folds[i][1]
+
+        train_folds = [folds[j][0] for j in range(k) if j != i]
+        train_data = []
+        for train_fold in train_folds:
+            for features in train_fold:
+                train_data.append(features)
+        train_data = np.array(train_data)  # converstion to np array
+        # TODO chekc in debugger
+        train_labels = []
+        for j in range(k):
+            if j != i:
+                for train_label in folds[j][1]:
+                    train_labels.append(train_label)
+
+        # run groups with classifier
+        classifier = classifier_factory.train(train_data, train_labels)
+        res_list = [classifier.classify(features) for features in test_data]
+
+        '''
+        classify each classify result when True means subject is actually sick
+        and res_list = 0 means classified as sick 
+        '''
+        test_false_positive = 0
+        test_false_negative = 0
+        test_true_positive = 0
+        test_true_negative = 0
+        N = len(res_list)
+        for j in range(N):
+            if res_list[j] == 0 and test_labels[j] == True:
+                test_true_positive += 1
+            elif res_list[j] == 0 and test_labels[j] == False:
+                test_false_positive += 1
+            elif res_list[j] == 1 and test_labels[j] == True:
+                test_false_negative += 1
+            elif res_list[j] == 1 and test_labels[j] == False:
+                test_true_negative += 1
+
+        accuracies.append((test_true_positive + test_true_negative) / N)
+        errors.append((test_false_positive + test_false_negative) / N)
+
+    return np.average(accuracies), np.average(errors)
