dc7bbebdec8217c9f035bc8d61c82800edf70f6a
Oran Nahoum <saintoran@gmail.com>
2019-01-05 16:45:20 +0200

    finished chapter 6 hopefully

--- run_evaluate.py
+++ run_evaluate.py
@@ -4,12 +4,15 @@ import classifier
 data = hw3_utils.load_data()
 ks = [1, 3, 5, 7, 13]
 
-for k in ks:
-    knn_fac = classifier.knn_factory(k)
-    accuracy, error = hw3_utils.evaluate(knn_fac, 2)
-    with open('experiments6.csv', 'w+') as result_file:
-        line = str(k) + ',' + str(accuracy) + ',' + str(error)
+with open('experiments6.csv', 'w') as _:
+    pass
+
+with open('experiments6.csv', 'w+') as result_file:
+    for k in ks:
+        knn_fac = classifier.knn_factory(k)
+        accuracy, error = hw3_utils.evaluate(knn_fac, 2)
+        line = str(k) + ',' + str(accuracy) + ',' + str(error) + '\n'
         result_file.write(line)
-    print("finished: ", k)
+        print("finished: ", k)
 
 print("FINISHED")
